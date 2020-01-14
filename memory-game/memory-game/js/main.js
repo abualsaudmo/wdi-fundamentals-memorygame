49013acb819a3d8dc7b63cd5ff95bbb021eacb52
Mohammed Abualsaud <abualsaudmo@gmail.com>
2020-01-14 04:53:37 +0300

    created game logic

--- memory-game/memory-game/js/main.js
+++ memory-game/memory-game/js/main.js
@@ -2,26 +2,24 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-//console.log(cardsInPlay);
-
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]){
-alert("You found a match!")
+function checkForMatch(){
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
 } else {
-	alert("Sorry, try again.")
+  console.log("Sorry, try again.");
 }
-} 
+}
+
+function flipcard(cardID){
+
+console.log("User flipped " + cards[cardID]);
+cardsInPlay.push(cards[cardID]);
+if (cardsInPlay.length === 2) {
+	checkForMatch();
+}
+}
+
+flipcard(0);
+flipcard(2);
 
 
